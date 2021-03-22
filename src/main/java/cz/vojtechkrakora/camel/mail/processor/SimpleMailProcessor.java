ceb8e6465cb65529fb19547b7257c9a6392a390a
Vojtěch Krákora <vojtech.krakora@wesecon.cz>
2021-03-22 22:15:15 +0100

    adds sending a png image from resources as an e-mail attachment

--- src/main/java/cz/vojtechkrakora/camel/mail/processor/SimpleMailProcessor.java
+++ src/main/java/cz/vojtechkrakora/camel/mail/processor/SimpleMailProcessor.java
@@ -4,8 +4,15 @@ import cz.vojtechkrakora.camel.mail.enums.Templates;
 import cz.vojtechkrakora.camel.mail.util.TemplateProviderUtil;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.apache.camel.attachment.AttachmentMessage;
+import org.springframework.core.io.ClassPathResource;
+
+import javax.activation.DataHandler;
+import javax.mail.util.ByteArrayDataSource;
 
 public class SimpleMailProcessor implements Processor {
+    public static final String ATTACHMENT_EXAMPLE_PATH = "attachment/ok_small.png";
+    public static final String ATTACHMENT_EXAMPLE_TYPE = "image/png";
     @Override
     public void process(Exchange exchange) throws Exception {
         exchange.getIn().setHeader("subject", "hello from camel");
@@ -13,5 +20,9 @@ public class SimpleMailProcessor implements Processor {
         exchange.getIn().setHeader("From", "no-reply@some.org");
         exchange.getIn().setHeader("To", "test@some.org");
         exchange.getIn().setBody(TemplateProviderUtil.getTemplateContent(Templates.SIMPLE));
+        AttachmentMessage am = exchange.getMessage(AttachmentMessage.class);
+        ClassPathResource attachment = new ClassPathResource(ATTACHMENT_EXAMPLE_PATH);
+        ByteArrayDataSource ba = new ByteArrayDataSource(attachment.getInputStream(), ATTACHMENT_EXAMPLE_TYPE);
+        am.addAttachment("ok.png", new DataHandler(ba));
     }
 }
